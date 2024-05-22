61cd8fd45f7057bb07592a2cf334c9e17fbecbe7
edd0126 <eduardo_schluff97@hotmail.com>
2024-05-22 23:45:19 +0100

    Se agrego encabezado

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import streamlit as st
 car_data = pd.read_csv("vehicles_us.csv") # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
 disp_checkbox = st.checkbox('Construir grafica de dispersion')
+
+st.header("Analisis de Datos vehicles_us.csv")
         
 if hist_button: # al hacer clic en el botón
         # escribir un mensaje
